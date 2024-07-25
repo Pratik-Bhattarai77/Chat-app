868638b444b8b28d36fd53289ea19ce1253f15ca
pratik <pratikrock42@gamil.com>
2024-07-25 19:33:07 +0545

    Deployment ready

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,9 @@
 //packages import
+import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+
 //routes import
 import authRoutes from "./routes/auth.routes.js";
 import userRoutes from "./routes/user.routes.js";
@@ -11,11 +13,11 @@ import messageRoutes from "./routes/message.routes.js";
 import connectToMongo from "./Data/connecttoMongos.js";
 import { app, server } from "./socket/socket.js";
 
-
-const app = express();
-const PORT = process.env.PORT || 5000;
-
 dotenv.config();
+
+const __dirname = path.resolve();
+
+const PORT = process.env.PORT || 5000;
 
 app.use(express.json()); // for parsing application/json (from req.body)
 app.use(cookieParser());
@@ -25,6 +27,11 @@ app.use("/api/auth",authRoutes)
 app.use("/api/messages",messageRoutes)
 app.use("/api/users",userRoutes)
 
+app.use(express.static(path.join(__dirname,"/frontend/dist")));
+
+app.get("*", (req, res) => {
+	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 
 
 server.listen(PORT,()=> {
